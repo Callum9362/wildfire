6c61d4d1251d8d4839a51b4ff6fdb49a1ceee769
Callum9362 <callum.gill.1993.cg@gmail.com>
2020-11-22 08:09:28 +1100

    Added the location marker component to be used to display fires

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,7 @@
 import GoogleMapReact from 'google-map-react'
+import LocationMarker from './LocationMarker'
 const API_KEY = process.env.REACT_APP_API_KEY;
+
 
 const Map = ({ center, zoom }) => {
     return (
@@ -9,6 +11,7 @@ const Map = ({ center, zoom }) => {
               defaultCenter= { center }
               defaultZoom= { zoom }
             >
+                <LocationMarker lat={center.lat} lng={center.lng} />
 
             </GoogleMapReact>
         </div>
