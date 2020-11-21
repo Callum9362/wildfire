8df01a28486128768c246af59228877340214ded
Callum9362 <callum.gill.1993.cg@gmail.com>
2020-11-22 08:53:01 +1100

    Add a location info box component so a user can click on an event and see its information

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,14 +1,24 @@
+import { useState } from 'react'
 import GoogleMapReact from 'google-map-react'
 import LocationMarker from './LocationMarker'
+import LocationInfoBox from './LocationInfoBox'
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 
 const Map = ({ eventData, center, zoom }) => {
 
+    const [locationInfo, setLocationInfo] = useState(null)
+
+
+
     const markers = eventData.map(ev => {
         if(ev.categories[0].id === 8){
 
-            return <LocationMarker lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]} />
+            return <LocationMarker 
+            lat={ev.geometries[0].coordinates[1]} 
+            lng={ev.geometries[0].coordinates[0]} 
+            onClick={() => setLocationInfo({ id: ev.id, title: ev.title})}
+            />
 
         }
         return null
@@ -27,6 +37,7 @@ const Map = ({ eventData, center, zoom }) => {
                 
 
             </GoogleMapReact>
+            {locationInfo && <LocationInfoBox info={locationInfo} />}
         </div>
     )
 }
